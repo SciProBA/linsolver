f23aba0515114f3a1645d014495698388db077dc
Bálint Aradi <aradi@uni-bremen.de>
2023-06-08 12:19:51 +0200

    Make stylistic changes to maximize pylint score

--- solvers.py
+++ solvers.py
@@ -2,26 +2,26 @@
 import numpy as np
 
 
-def gaussian_eliminate(aa, bb):
+def gaussian_eliminate(coeffs, rhs):
     """Solves a linear system of equations (Ax = b) by Gauss-elimination
 
     Args:
-        aa: Matrix with the coefficients. Shape: (n, n).
-        bb: Right hand side of the equation. Shape: (n,)
+        coeffs: Matrix with the coefficients. Shape: (n, n).
+        rhs: Right hand side of the equation. Shape: (n,)
 
     Returns:
-        Vector xx with the solution of the linear equation or None
-        if the equations are linearly dependent.
+        Vector xx with the solution of the linear equation or None if the equations are linearly
+        dependent.
     """
-    nn = aa.shape[0]
+    nn = coeffs.shape[0]
     for ii in range(nn - 1):
         for jj in range(ii + 1, nn):
-            coeff = -aa[jj, ii] / aa[ii, ii]
-            aa[jj, ii:] += coeff * aa[ii, ii:]
-            bb[jj] += coeff * bb[ii]
+            coeff = -coeffs[jj, ii] / coeffs[ii, ii]
+            coeffs[jj, ii:] += coeff * coeffs[ii, ii:]
+            rhs[jj] += coeff * rhs[ii]
 
     xx = np.empty((nn,), dtype=float)
     for ii in range(nn - 1, -1, -1):
         # Note: dot product of two arrays of zero size is 0.0
-        xx[ii] = (bb[ii] - aa[ii, ii + 1:] @ xx[ii + 1:]) / aa[ii, ii]
+        xx[ii] = (rhs[ii] - coeffs[ii, ii + 1 :] @ xx[ii + 1 :]) / coeffs[ii, ii]
     return xx
